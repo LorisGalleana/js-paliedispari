8c04f6ead1eb8a24707eed32155eead9b85137f3
LorisGalleana <loris.galleana@gmail.com>
2024-06-12 16:23:50 +0200

    feat create oddEven function

--- myscript.js
+++ myscript.js
@@ -31,31 +31,21 @@ function isPalindrome() {
 
 
 
-//chiediamo all'utente di scegliere tra pari e dispari
-//let pariOdispari = prompt("scegli pari o dispari");
-let pari = "pari"
-let dispari = "dispari"
+//chiediamo all'utente di scegliere tra pari e dispari e controlliamo che possa rispondere solo con pari o dispari
 let checkCondition = false;
-console.log(checkCondition);
 
 while (checkCondition == false) {
-    console.log(checkCondition);
     pariOdispari = prompt("scegli pari o dispari");
-    if (pariOdispari === pari) {
+    if (pariOdispari === "pari") {
         checkCondition = true;
-        console.log(checkCondition);
     }
-    else if (pariOdispari === dispari) {
+    else if (pariOdispari === "dispari") {
         checkCondition = true;
-        console.log(checkCondition);
     }
     else {
         checkCondition = false;
-        console.log(checkCondition);
     }
 }
-
-
 //chiediamo all'utente di scegliere un numero tra 1 e 5
 let userNumber = parseInt(prompt("scegli un numero da 1 a 5"));
 //controlliamo che il numero sia effettivamente in quell'intervallo
@@ -68,6 +58,9 @@ console.log(pcNumber);
 //sommiamo i due numeri
 let somma = userNumber + pcNumber;
 console.log(somma);
+//controlliamo se il numero è pari o dispari
+let pari = oddEven(somma)
+console.log(pari);
 //se l'utente ha scelto pari e la somma è pari, vince l'utente, se no vince il computer
 
 
@@ -76,4 +69,8 @@ console.log(somma);
 function rangeNumber(max, min) {
     let number = Math.floor(Math.random() * (max - min + 1)) + min;
     return number
+}
+function oddEven(number) {
+    let even = (number % 2 === 0)
+    return even
 }
