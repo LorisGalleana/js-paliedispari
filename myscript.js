e34ebcd120ac1020a158f8f9e998d0afba4782de
LorisGalleana <loris.galleana@gmail.com>
2024-06-12 16:16:21 +0200

    fix check condition pari or dispari

--- myscript.js
+++ myscript.js
@@ -32,7 +32,30 @@ function isPalindrome() {
 
 
 //chiediamo all'utente di scegliere tra pari e dispari
-let pariOdispari = prompt("scegli pari o dispari");
+//let pariOdispari = prompt("scegli pari o dispari");
+let pari = "pari"
+let dispari = "dispari"
+let checkCondition = false;
+console.log(checkCondition);
+
+while (checkCondition == false) {
+    console.log(checkCondition);
+    pariOdispari = prompt("scegli pari o dispari");
+    if (pariOdispari === pari) {
+        checkCondition = true;
+        console.log(checkCondition);
+    }
+    else if (pariOdispari === dispari) {
+        checkCondition = true;
+        console.log(checkCondition);
+    }
+    else {
+        checkCondition = false;
+        console.log(checkCondition);
+    }
+}
+
+
 //chiediamo all'utente di scegliere un numero tra 1 e 5
 let userNumber = parseInt(prompt("scegli un numero da 1 a 5"));
 //controlliamo che il numero sia effettivamente in quell'intervallo
@@ -41,14 +64,11 @@ while (userNumber < 1 || userNumber > 5) {
 }
 //generiamo un numero random da 1 a 5 e lo assegniamo al computer
 let pcNumber = rangeNumber(5, 1);
-
+console.log(pcNumber);
 //sommiamo i due numeri
 let somma = userNumber + pcNumber;
-
+console.log(somma);
 //se l'utente ha scelto pari e la somma è pari, vince l'utente, se no vince il computer
-
-
-
 
 
 
